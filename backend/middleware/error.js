d2c8f68627fccb0b59c49618c0100a4f29c6f6dd
zzzzz <m-860935@moe-dl.edu.my>
2025-09-01 14:56:05 +0000

    deleted

--- backend/middleware/error.js
+++
@@ -1,8 +0,0 @@
-// middleware/error.js
-export const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(err.statusCode || 500).json({
-        success: false,
-        message: err.message || "Server Error",
-    });
-};
--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-// Example route
-router.get("/", (req, res) => {
-    res.json({ message: "Users route working!" });
-});
-
-export default router;
