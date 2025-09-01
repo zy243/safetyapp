201b350a44a37054d37ea2461aaafa949980c370
zzzzz <m-860935@moe-dl.edu.my>
2025-09-01 03:07:20 +0000

    connect backend

--- frontend/app/App.js
+++ frontend/app/App.js
@@ -1,14 +1,20 @@
-// app/App.js
-import React from 'react';
-import { GuardianProvider } from '../contexts/GuardianContext';
-import 'expo-router/entry'; // Automatically handles routing
+import React, { useEffect } from "react";
+import { Text, View } from "react-native";
+import Constants from "expo-constants";
 
-const App = () => {
+export default function App() {
+  const BACKEND_URL = Constants.manifest.extra.BACKEND_URL;
+
+  useEffect(() => {
+    fetch(`${BACKEND_URL}/api/users`)
+      .then(res => res.json())
+      .then(data => console.log("Users from backend:", data))
+      .catch(err => console.error("Error fetching users:", err));
+  }, []);
+
   return (
-    <GuardianProvider>
-      {/* Expo Router will handle navigation automatically */}
-    </GuardianProvider>
+    <View>
+      <Text>Check console for backend response</Text>
+    </View>
   );
-};
-
-export default App;
+}
