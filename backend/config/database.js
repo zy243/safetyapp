9622d804b95f2e47123cee274a96fa86e95753bd
zzzzz <m-860935@moe-dl.edu.my>
2025-09-21 15:13:50 +0000

    correct database

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/unisafe', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI , {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
